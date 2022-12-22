e87ff5776de325bd923548014223626a8c52fa68
KevinEscobarDeveloper <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2022-12-22 11:13:58 -0600

    corrections in production header

--- src/components/layout/private/Header.jsx
+++ src/components/layout/private/Header.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import {Nav} from "react-router-dom"
 
 export const Header = () => {
   return (
     <header className="layout__navbar">
-      <Nav>
       <div className="navbar__header">
         <a href="#" className="navbar__title">
           REACTSOCIAL
         </a>
       </div>
-      <Nav/>
     </header>
   );
 };

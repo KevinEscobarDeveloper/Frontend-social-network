ae5c0fa25c0bae2b4c9984fa075277324f7a040b
KevinEscobarDeveloper <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2022-12-22 11:11:22 -0600

    corrections in production

--- src/components/layout/private/Header.jsx
+++ src/components/layout/private/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Nav } from "./nav";
+import {Nav} from "react-router-dom"
 
 export const Header = () => {
   return (
     <header className="layout__navbar">
+      <Nav>
       <div className="navbar__header">
         <a href="#" className="navbar__title">
           REACTSOCIAL
